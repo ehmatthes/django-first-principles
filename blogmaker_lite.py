bf6eb6b318a0347337d25c6cef9f2dec5b6d1a1d
Eric Matthes <ehmatthes@gmail.com>
2024-04-17 10:12:12 -0800

    Home page uses a template.

--- blogmaker_lite.py
+++ blogmaker_lite.py
@@ -1,24 +1,26 @@
+from pathlib import Path
+
 from django.conf import settings
-from django.http import HttpResponse
 from django.urls import path
 from django.core.handlers.wsgi import WSGIHandler
 from django.core.management import \
     execute_from_command_line
+from django.shortcuts import render
 
 settings.configure(
     ROOT_URLCONF=__name__,
     DEBUG=True,
     SECRET_KEY="my-secret-key",
+    TEMPLATES=[
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": [Path(__file__).parent / "templates"],
+        }
+    ],
 )
 
 def index(request):
-    title = "BlogMaker Lite"
-    description = "Start a blog!"
-
-    page_text = f"<h1>{title}</h1>"
-    page_text += f"<p>{description}</p>"
-
-    return HttpResponse(page_text)
+    return render(request, "index.html")
 
 urlpatterns = [
     path("", index)
