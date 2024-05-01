a406561cc77fae541939f63100427d572b1778f1
Eric Matthes <ehmatthes@gmail.com>
2024-05-01 07:07:34 -0800

    Registered Blog with admin.

--- blogmaker_lite.py
+++ blogmaker_lite.py
@@ -3,7 +3,12 @@ from pathlib import Path
 from django.urls import path
 from django.core.handlers.wsgi import WSGIHandler
 from django.shortcuts import render
+from django.contrib import admin
 
+from blogs.models import Blog
+
+
+admin.site.register(Blog)
 
 def index(request):
     return render(request, "index.html")
