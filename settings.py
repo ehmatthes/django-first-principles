01c8dad284aa437260ccabe80a5bdcf0a17a601e
Eric Matthes <ehmatthes@gmail.com>
2024-05-01 07:05:05 -0800

    Updated settings to use admin site.

--- settings.py
+++ settings.py
@@ -10,10 +10,32 @@ TEMPLATES=[
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [Path(__file__).parent / "templates"],
+        "APP_DIRS": True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     }
 ]
 
-INSTALLED_APPS=["blogs"]
+INSTALLED_APPS=[
+    "blogs",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    'django.contrib.sessions',
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
 
 DATABASES={
     'default': {
@@ -22,3 +44,5 @@ DATABASES={
     }
 }
 DEFAULT_AUTO_FIELD="django.db.models.BigAutoField"
+
+STATIC_URL="static/"
