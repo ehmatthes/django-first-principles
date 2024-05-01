6d251970943e37cc4e3016ec12e7c9dccf9a2ad4
Eric Matthes <ehmatthes@gmail.com>
2024-05-01 06:15:24 -0800

    Moved code for running commands to manage.py.

--- blogmaker_lite.py
+++ blogmaker_lite.py
@@ -1,17 +1,9 @@
 from pathlib import Path
-import os
 
-import django
 from django.urls import path
 from django.core.handlers.wsgi import WSGIHandler
-from django.core.management import \
-    execute_from_command_line
 from django.shortcuts import render
 
-
-# Load settings.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-django.setup()
 
 def index(request):
     return render(request, "index.html")
@@ -21,6 +13,3 @@ urlpatterns = [
 ]
 
 application = WSGIHandler()
-
-if __name__ == "__main__":
-    execute_from_command_line()
