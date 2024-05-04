15cd7fe95be0487679b72ffb3ebb155d06263f56
Eric Matthes <ehmatthes@gmail.com>
2024-05-04 13:40:05 -0800

    Added BlogPost model.

--- blogs/models.py
+++ blogs/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=500)
+    body = models.TextField()
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title[:50]
