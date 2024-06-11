b1481ae18dfa8952d311b0fb5a2cc161148bb7f6
Eric Matthes <ehmatthes@gmail.com>
2024-06-11 09:28:50 -0800

    Blog view function and template.

--- blogmaker_lite.py
+++ blogmaker_lite.py
@@ -18,6 +18,16 @@ def blogs(request):
     context = {"blogs": all_blogs}
     return render(request, "blogs.html", context)
 
+def blog(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    posts = blog.blogpost_set.all()
+    
+    context = {
+        "blog": blog,
+        "posts": posts,
+    }
+    return render(request, "blog.html", context)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("blogs/", blogs, name="blogs"),
