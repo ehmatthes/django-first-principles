691a61893646dd98fa565d9df7c4ba856ce59d28
Eric Matthes <ehmatthes@gmail.com>
2024-06-22 17:28:48 -0800

    Script generates fake superuser.

--- generate_sample_data.py
+++ generate_sample_data.py
@@ -12,3 +12,13 @@ django.setup()
 # Flush current data.
 call_command("flush", "--noinput")
 print("Flushed existing db.")
+
+# Create a superuser.
+os.environ["DJANGO_SUPERUSER_PASSWORD"] = "fake_pw"
+
+cmd = "createsuperuser --username fake_admin"
+cmd += " --email fake_email@example.com"
+cmd += " --noinput"
+
+cmd_parts = cmd.split()
+call_command(*cmd_parts)
