ddf8ea329d959d5fc3b2471232f0f9c2da431d2d
Eric Matthes <ehmatthes@gmail.com>
2024-06-22 17:53:29 -0800

    Each blog has a different description.

--- model_factories.py
+++ model_factories.py
@@ -8,9 +8,12 @@ fake = Faker()
 def get_title():
     return " ".join(fake.words()).title()
 
+def get_description():
+    return fake.sentence(nb_words=10)
+
 class BlogFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Blog
 
     title = factory.LazyFunction(get_title)
-    description = "This is a great sample blog!"
+    description = factory.LazyFunction(get_description)
