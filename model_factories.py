79e4bafd80de73d30f2869df5e5aabbe0395f728
Eric Matthes <ehmatthes@gmail.com>
2024-06-22 17:52:55 -0800

    Generates 10 blogs, with different random names.

--- model_factories.py
+++ model_factories.py
@@ -5,9 +5,12 @@ from blogs.models import Blog, BlogPost
 
 fake = Faker()
 
+def get_title():
+    return " ".join(fake.words()).title()
+
 class BlogFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Blog
 
-    title = " ".join(fake.words()).title()
+    title = factory.LazyFunction(get_title)
     description = "This is a great sample blog!"
