4a3be9ba69d89307a3702e06d135b2e89ea1edf4
Eric Matthes <ehmatthes@gmail.com>
2024-07-13 08:41:33 -0800

    Added owner to Blog model.

--- blogs/models.py
+++ blogs/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
